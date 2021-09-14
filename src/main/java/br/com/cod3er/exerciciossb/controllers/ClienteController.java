921867b4e90bb36b29319069a6a616daf41d0e19
Roberto Coutinho <robertogmcfilho@gmail.com>
2021-09-14 18:49:31 -0300

    criação de metodos para a passagem de parametros via path e querryparam.

--- src/main/java/br/com/cod3er/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3er/exerciciossb/controllers/ClienteController.java
@@ -1,16 +1,26 @@
 package br.com.cod3er.exerciciossb.controllers;
 
 import br.com.cod3er.exerciciossb.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/clientes")
 public class ClienteController {
 
-    @GetMapping(path = "/lista")
+    @GetMapping(path = "/qualquer")
     public Cliente obterCliente(){
         return new Cliente(2, "Roberto", "98797454612");
     }
+
+    @GetMapping("/{id}")
+    public Cliente obterClieteId1(@PathVariable int id){
+        return new Cliente(id, "roberto", "56465456132");
+    }
+
+    @GetMapping
+    public Cliente obterClieteId2(@RequestParam(name = "id") int id){
+        return new Cliente(id, "roberto", "56465456132");
+    }
+
+
 }
