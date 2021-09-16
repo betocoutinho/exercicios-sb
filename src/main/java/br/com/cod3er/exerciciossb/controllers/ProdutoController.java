99e3e9dd33b8c268b379e0f5acd60bbed29579f2
Roberto Coutinho <robertogmcfilho@gmail.com>
2021-09-16 17:52:11 -0300

    adicionados os os metodos de obter, atualizar e deletar produtos.

--- src/main/java/br/com/cod3er/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3er/exerciciossb/controllers/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -14,9 +15,34 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
+    //@PostMapping
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
     public @ResponseBody  Produto novoProduto(@Valid Produto produto){
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping
+    public Iterable<Produto> obterProduto(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
+
+    /*
+    @PutMapping
+    public Produto alterarProduto(@Valid Produto produto){
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+     */
+
+    @DeleteMapping("/{id}")
+    public void deletarProduto(@PathVariable int id){
+        produtoRepository.deleteById(id);
     }
 }
