5bc574232e0f54ac46ad388a1e42dc011d3fa901
Roberto Coutinho <robertogmcfilho@gmail.com>
2021-09-16 18:10:54 -0300

    Criando metodo de paginação de dados

--- src/main/java/br/com/cod3er/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3er/exerciciossb/controllers/ProdutoController.java
@@ -3,6 +3,8 @@ package br.com.cod3er.exerciciossb.controllers;
 import br.com.cod3er.exerciciossb.model.entities.Produto;
 import br.com.cod3er.exerciciossb.model.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -25,6 +27,15 @@ public class ProdutoController {
     @GetMapping
     public Iterable<Produto> obterProduto(){
         return produtoRepository.findAll();
+    }
+
+    // {n} posição na pagina
+    //{q} quantidade de elementos na pagina
+    @GetMapping(path = "/p/{n}/{q}")
+    public Iterable<Produto> obterProdutoPorPagina(@PathVariable int n ,
+                                                   @PathVariable int q ){
+        Pageable pageable = PageRequest.of(n, q);
+        return produtoRepository.findAll(pageable);
     }
 
     @GetMapping("/{id}")
