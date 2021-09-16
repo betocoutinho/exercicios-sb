a084bb82e3496a3d08a385129151a0ee72ec237c
Roberto Coutinho <robertogmcfilho@gmail.com>
2021-09-16 16:04:06 -0300

    Correção da classe produto e simplificação

--- src/main/java/br/com/cod3er/exerciciossb/model/entities/Produto.java
+++ src/main/java/br/com/cod3er/exerciciossb/model/entities/Produto.java
@@ -13,13 +13,16 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
-
-    public Produto(String nome) {
-        this.nome = nome;
-    }
+    private double preco;
+    private double desconto;
 
     public Produto() {
+    }
 
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
     }
 
     public int getId() {
@@ -36,5 +39,21 @@ public class Produto {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
     }
 }
